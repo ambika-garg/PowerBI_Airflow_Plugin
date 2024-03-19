908927301c78cd1be047ba129747406548eaf11d
Ambika Garg <v-ambikagarg@microsoft.com>
2024-03-19 17:45:43 -0400

    operator

--- plugins/operators/powerbi_refresh_dataset_operator.py
+++ plugins/operators/powerbi_refresh_dataset_operator.py
@@ -9,6 +9,7 @@ class PowerBILink(BaseOperatorLink):
     name = "Power BI"
 
     def get_link(self, operator: BaseOperator, *, ti_key: TaskInstanceKey):
+        print(operator)
         return "https://app.powerbi.com/groups/effb3465-0270-42ec-857a-0b2c9aafce46/datasets/372d46ba-e761-4c9e-b306-5d7d89676b13/details?experience=power-bi"
 
 
