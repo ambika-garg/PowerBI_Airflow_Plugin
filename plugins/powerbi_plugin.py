7fd6ad855ff691f1b07afbe03d857f3a5b650c19
Ambika Garg <v-ambikagarg@microsoft.com>
2024-03-19 16:22:03 -0400

    xcom integration compatibility

--- plugins/powerbi_plugin.py
+++ plugins/powerbi_plugin.py
@@ -1,10 +1,10 @@
 from airflow.plugins_manager import AirflowPlugin
 
-from operators.powerbi_refresh_dataset_operator import GoogleLink
+from operators.powerbi_refresh_dataset_operator import PowerBILink
 
 # Defining the plugin class
 class AirflowExtraLinkPlugin(AirflowPlugin):
     name = "powerbi_link_plugin"
     operator_extra_links = [
-        GoogleLink(),
+        PowerBILink(),
     ]
