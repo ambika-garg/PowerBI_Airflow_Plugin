82db3106075e9161295fa647073a3ed7088bbb05
Ambika Garg <v-ambikagarg@microsoft.com>
2024-03-21 15:19:39 -0400

    debugging extra link class

--- plugins/operators/powerbi_refresh_dataset_operator.py
+++ plugins/operators/powerbi_refresh_dataset_operator.py
@@ -19,6 +19,9 @@ class PowerBILink(BaseOperatorLink):
         group_id = operator.group_id
         dataset_id = operator.dataset_id
 
+        print("Group Id: ", group_id)
+        print("Dataset Id: ", dataset_id)
+
         return f"https://app.powerbi.com/groups/{group_id}/datasets/{dataset_id}/details?experience=power-bi"
 
 
