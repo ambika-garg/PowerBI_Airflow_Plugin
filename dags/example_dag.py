4c3ca82318d5f01e91882c2e9e49a7e442e5135a
Ambika Garg <v-ambikagarg@microsoft.com>
2024-04-27 22:16:44 -0400

    Fix import to work in ADF WOM

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # The DAG object
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from plugins.operators.powerbi_refresh_dataset_operator import PowerBIDatasetRefreshOperator
+from powerbi_refresh_dataset_operator import PowerBIDatasetRefreshOperator
 
 
 with DAG(
